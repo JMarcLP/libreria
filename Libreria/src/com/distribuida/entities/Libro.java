875198da7d371d3ac65f710a27d20ead99d1c9ed
Tecnico <Tecnico@DESKTOP-B489OFK.lan>
2024-05-31 12:25:53 -0500

    update

---
+++ Libreria/src/com/distribuida/entities/Libro.java
@@ -0,0 +1,189 @@
+package com.distribuida.entities;
+
+import java.util.Date;
+
+public class Libro {
+	
+	//atributos
+	private int idLibro;
+	private String titutlo;
+	private String editorial;
+	private int numPaginas;
+	private String edicion;
+	private String idioma;
+	private Date fechaPublicacion;
+	private String descripcion;
+	private String tipoPasta;
+	private String isbn;
+	private String numEjemplares;
+	private String portada;
+	private String presentacion;
+	private double precio;
+	private int idAutor;
+	private int idCategoria;
+	
+	//constructores
+	public Libro(int idLibro, String titutlo, String editorial, int numPaginas, String edicion, String idioma,
+			Date fechaPublicacion, String descripcion, String tipoPasta, String isbn, String numEjemplares,
+			String portada, String presentacion, double precio, int idAutor, int idCategoria) {
+		this.idLibro = idLibro;
+		this.titutlo = titutlo;
+		this.editorial = editorial;
+		this.numPaginas = numPaginas;
+		this.edicion = edicion;
+		this.idioma = idioma;
+		this.fechaPublicacion = fechaPublicacion;
+		this.descripcion = descripcion;
+		this.tipoPasta = tipoPasta;
+		this.isbn = isbn;
+		this.numEjemplares = numEjemplares;
+		this.portada = portada;
+		this.presentacion = presentacion;
+		this.precio = precio;
+		this.idAutor = idAutor;
+		this.idCategoria = idCategoria;
+	}
+
+	//metodos getters and setters
+	public int getIdLibro() {
+		return idLibro;
+	}
+
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+
+	public String getTitutlo() {
+		return titutlo;
+	}
+
+	public void setTitutlo(String titutlo) {
+		this.titutlo = titutlo;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+
+	public int getNumPaginas() {
+		return numPaginas;
+	}
+
+	public void setNumPaginas(int numPaginas) {
+		this.numPaginas = numPaginas;
+	}
+
+	public String getEdicion() {
+		return edicion;
+	}
+
+	public void setEdicion(String edicion) {
+		this.edicion = edicion;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public Date getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(Date fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getTipoPasta() {
+		return tipoPasta;
+	}
+
+	public void setTipoPasta(String tipoPasta) {
+		this.tipoPasta = tipoPasta;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getNumEjemplares() {
+		return numEjemplares;
+	}
+
+	public void setNumEjemplares(String numEjemplares) {
+		this.numEjemplares = numEjemplares;
+	}
+
+	public String getPortada() {
+		return portada;
+	}
+
+	public void setPortada(String portada) {
+		this.portada = portada;
+	}
+
+	public String getPresentacion() {
+		return presentacion;
+	}
+
+	public void setPresentacion(String presentacion) {
+		this.presentacion = presentacion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	//to string
+	@Override
+	public String toString() {
+		return "Libro [idLibro=" + idLibro + ", titutlo=" + titutlo + ", editorial=" + editorial + ", numPaginas="
+				+ numPaginas + ", edicion=" + edicion + ", idioma=" + idioma + ", fechaPublicacion=" + fechaPublicacion
+				+ ", descripcion=" + descripcion + ", tipoPasta=" + tipoPasta + ", isbn=" + isbn + ", numEjemplares="
+				+ numEjemplares + ", portada=" + portada + ", presentacion=" + presentacion + ", precio=" + precio
+				+ ", idAutor=" + idAutor + ", idCategoria=" + idCategoria + "]";
+	}
+	
+	
+	
+	
+}
