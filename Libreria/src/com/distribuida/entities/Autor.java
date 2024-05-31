466b265f4311357738121e2dd63eea4c9448547d
Tecnico <Tecnico@DESKTOP-B489OFK.lan>
2024-05-31 11:55:53 -0500

    update

--- Libreria/src/com/distribuida/entities/Autor.java
+++ Libreria/src/com/distribuida/entities/Autor.java
@@ -1,5 +1,97 @@
 package com.distribuida.entities;
 
 public class Autor {
+	
+	//atributos
+	private int idAutor;
+	private String nombre;
+	private String apellido;
+	private String pais;
+	private String direccion;
+	private String telefono;
+	private String correo;
+	
+	//constructores
+	public Autor(int idAutor, String nombre, String apellido, String pais, String direccion, String telefono,
+			String correo) {
+		super();
+		this.idAutor = idAutor;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.pais = pais;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.correo = correo;
+	}
+	
+	
+
+	//metodos getters and setters
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+
+
+	//to string
+	@Override
+	public String toString() {
+		return "Autor [idAutor=" + idAutor + ", nombre=" + nombre + ", apellido=" + apellido + ", pais=" + pais
+				+ ", direccion=" + direccion + ", telefono=" + telefono + ", correo=" + correo + "]";
+	}
+	
+	
 
 }
